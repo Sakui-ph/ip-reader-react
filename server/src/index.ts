1fce3ffb0b4d461ab9568270bbb655d96c449fef
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2024-10-29 19:07:45 +0800

    remove boilerplate stuff leftover

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,10 +12,6 @@ const corsOptions = {
   methods: ["GET", "POST", "DELETE", "PATCH"],
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
