e056fa2a252823ea77e55595272eed66795dfae9
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2024-10-29 21:01:14 +0800

    uncomplete push for submission

--- server/src/controllers/ipinfo.ts
+++ server/src/controllers/ipinfo.ts
@@ -3,8 +3,10 @@ import { Request, Response } from "express";
 module.exports = {
   getUserIP: async (req: Request, res: Response) => {
     let url = "https://ipinfo.io/geo";
-    if (req.query.ip !== null) {
+    if (req.query.ip !== undefined) {
+      console.log("ip found!");
       url = `https://ipinfo.io/${req.body.ip}/geo`;
+      console.log(url);
     }
 
     const headers = new Headers();
